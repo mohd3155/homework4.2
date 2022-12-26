06feae84052733912092271ee444c1d8e10f9127
Mohammed <moh9_kamal@hotmail.com>
2022-12-26 21:57:04 +0200

    solve homework 4-2

---
+++ home work 4-2.py
@@ -0,0 +1,32 @@
+
+num1 = input("please enter the first number")
+
+if num1.isdigit():
+    opr = input("please choose an operation")
+    if opr == '1' or opr == '+':
+        num2 = input("Please enter the second number")
+        print(float(num1) + float(num2))
+
+    elif opr == '2' or opr == '-':
+        num2 = input("Please enter the second number")
+        print(float(num1) - float(num2))
+
+    elif opr == '3' or opr == '*':
+        num2 = input("Please enter the second number")
+        print(float(num1) * float(num2))
+
+    elif opr == '4' or opr == '/':
+        num2 = input("Please enter the second number")
+        print(float(num1) / float(num2))
+
+    elif opr == '5' or opr == '^':
+        num2 = input("Please enter the second number")
+        print(float(num1) ^ float(num2))
+
+    elif opr == '6' or opr == '%':
+        num2 = input("Please enter the second number")
+        print(float(num1) % float(num2))
+
+else:
+    print("Error")
+
